47391861b6d975242024146b65e2ad59dcbdbccf
Poly <RAE@d.c>
2024-01-06 22:50:09 +0100

    Progression done

--- BYT-Implementation/Progression.cs
+++ BYT-Implementation/Progression.cs
@@ -1,6 +1,22 @@
-﻿namespace BYT_Implementation;
+﻿using System.IO.Enumeration;
+
+namespace BYT_Implementation;
 
 public class Progression
 {
+    // Fields
+    public string Filename { get; set; }
+    public string? Nickname { get; set; }
     
+    // Connection
+    public Game Game { get; set; }
+    public User User { get; set; }
+
+    public Progression(string filename, string? nickname, Game game, User user)
+    {
+        Filename = filename;
+        Nickname = nickname;
+        Game = game;
+        User = user;
+    }
 }
